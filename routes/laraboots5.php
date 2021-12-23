3d1a45d822348db6309c6b6e9bfb0bf0abc1d6d9
Dav0x0 <david1.aprilio4@gmail.com>
2021-12-24 00:17:21 +0700

    tes update

--- routes/laraboots5.php
+++ routes/laraboots5.php
@@ -3,6 +3,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/docs')->group(function () {
     Route::get('/forms', function() {
-        echo 'Hello Docs from the BS5 Component Laravel!';
+        echo 'Hello Docs from the Bootstrap5 Component Laravel';
     });
 });
