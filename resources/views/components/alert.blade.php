1736b7c3253dc4b1a138a1bbdc2d971c5f76d5e0
Dav0x0 <david1.aprilio4@gmail.com>
2021-12-27 09:28:33 +0700

    update Alert component

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,3 +1,17 @@
-<div class="alert alert-danger">
-    ada error
+@props([
+    'type' => 'success',
+    'error' => false,
+    'flashMessage' => false,
+    'head' => false,
+    'close' => false
+])
+<div class="alert alert-{{ $type }}" role="alert">
+    @if ($head)
+        <h4 class="alert-heading">{{ $head }}</h4>
+    @endif
+    {{ $slot }}
+
+    @if ($close)
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    @endif
 </div>
