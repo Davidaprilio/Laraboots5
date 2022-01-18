8e3be4e421276de04350c722027e1b3defd92eb4
Dav0x0 <david1.aprilio4@gmail.com>
2022-01-19 00:41:40 +0700

    update

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,33 +1,32 @@
 @props([
-  'label' => false,
-  'id' => Str::random(6),
-  'validation' => false,
-  'old' => false,
-  'value' => false,
-  'parentClass' => 'mb-3',
-  'name' => false,
-  'type' => 'text'
+'label' => false,
+'id' => Str::random(6),
+'validation' => false,
+'old' => false,
+'help' => false,
+'value' => false,
+'parentClass' => 'mb-3',
+'name' => false,
+'type' => 'text'
 ])
 
 <div class="{{ $parentClass }}">
   @if($label)
   <label for="{{ $id }}" class="form-label">{{ $label }}</label>
   @endif
-  <input
-    type="{{ $type }}"
-    value="{{ $value ?? old($name) }}"
-    id="{{ $id }}"
-    @if($name) name="{{ $name }}" @endif
-    {{ $attributes->class([
-      'form-control', 
-      'is-invalid' => ($validation && $errors->has($name)) ? $errors->has($name) : false
-      ]) }}
-    {{ $attributes }}>
+  <input type="{{ $type }}" value="{{ $value ?? old($name) }}" id="{{ $id }}" @if($name) name="{{ $name }}" @endif {{
+    $attributes->class([
+  'form-control',
+  'is-invalid' => ($validation && $errors->has($name)) ? $errors->has($name) : false
+  ]) }}
+  {{ $attributes }}>
   @if($validation)
-   @error($name)
-   <div class="invalid-feedback">
-      {{ $message }}
-    </div>
-    @enderror
+  @error($name)
+  <div class="invalid-feedback">
+    {{ $message }}
+  </div>
+  @else
+  @if ($help) <small class="text-muted">{{ $help }}</small> @endif
+  @enderror
   @endif
 </div>
--- routes/laraboots5.php
+++ routes/laraboots5.php
@@ -1,25 +1,32 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/docs')->middleware('web')->group(function () {
 
-    Route::get('/alert', function()
-    {
+    Route::get('/alert', function () {
         return view('laraboots5::docs.alert');
     });
 
-    Route::get('/forms', function()
-    {
+    Route::get('/forms', function () {
         return view('laraboots5::docs.forms');
     });
 
-    Route::post('/flash-message', function()
-    {
+    Route::post('/form', function () {
+        request()->validate([
+            'form1' => 'required|min:100',
+            'form2' => 'required|min:100',
+            'form3' => 'required|min:100',
+            'form4' => 'required|min:100',
+        ]);
+        return redirect()->back();
+    });
+
+    Route::post('/flash-message', function () {
         return redirect()->back()->with('flash-demo', 'Flash messages');
     })->name('flash');
 
-    Route::post('/error', function()
-    {
-        return redirect()->back()->withErrors(['error-demo' => 'Error messages']);   
+    Route::post('/error', function () {
+        return redirect()->back()->withErrors(['error-demo' => 'Error messages']);
     })->name('error');
 });
